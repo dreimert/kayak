f5e159d9837b467e6107280d42099b7909f3d840
Damien Reimert <damien.reimert@gmail.com>
2024-02-20 22:29:42 +0100

    Ajout d'information en cas d'erreur

--- api/src/index.ts
+++ api/src/index.ts
@@ -17,21 +17,6 @@ const app = express();
 
 passport.use(magicLogin);
 
-/* Configure session management.
- *
- * When a login session is established, information about the user will be
- * stored in the session.  This information is supplied by the `serializeUser`
- * function, which is yielding the user ID and username.
- *
- * As the user interacts with the app, subsequent requests will be authenticated
- * by verifying the session.  The same user information that was serialized at
- * session establishment will be restored when the session is authenticated by
- * the `deserializeUser` function.
- *
- * Since every request to the app needs the user ID and username, in order to
- * fetch todo records and render the user element in the navigation bar, that
- * information is stored in the session.
- */
 passport.serializeUser(function(user, cb) {
   process.nextTick(function() {
     cb(null, { id: user.id });
@@ -43,6 +28,7 @@ passport.deserializeUser(function(user, cb) {
     const find = User.findById(user.id);
 
     if (!find) {
+      console.error('User not found', user.id);
       return cb(new Error('User not found'));
     }
 
