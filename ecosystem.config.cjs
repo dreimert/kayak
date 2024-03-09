0ed8f012f601f5144c2745fb368af47dc3b97f83
Damien Reimert <damien.reimert@gmail.com>
2024-03-09 18:31:48 +0100

    Remove frontend from ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,17 +1,17 @@
 module.exports = {
   apps: [{
-    name: 'Kayakons - frontend',
-    script: 'dist/kayak-date/server/server.mjs',
-    listen_timeout: 10000,
-    autorestart: true,
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    },
-    time: true
-  }, {
+  //   name: 'Kayakons - frontend',
+  //   script: 'dist/kayak-date/server/server.mjs',
+  //   listen_timeout: 10000,
+  //   autorestart: true,
+  //   env: {
+  //     NODE_ENV: 'development'
+  //   },
+  //   env_production: {
+  //     NODE_ENV: 'production'
+  //   },
+  //   time: true
+  // }, {
     name: 'Kayakons - api',
     script: './api/dist/index.js',
     interpreter_args: '--env-file=.env',
